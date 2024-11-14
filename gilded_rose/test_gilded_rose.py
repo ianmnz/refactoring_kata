e44042707b75791df303b72f283e0b4b61d77f16
ianmnz <ianmemar@gmail.com>
2024-11-14 11:11:32 +0100

    Refactored for more precise tests

--- gilded_rose/test_gilded_rose.py
+++ gilded_rose/test_gilded_rose.py
@@ -1,12 +1,15 @@
 # Test file for gilded rose kata
 
 import io
+import pytest
+import random
 from itertools import product
 from pathlib import Path
 
 from gilded_rose.refactored import Item, GildedRose
 
 
+# @pytest.mark.skip(reason="")
 def test_golden_master():
     golden_master_file = Path('gilded_rose/golden_master.txt')
 
@@ -51,80 +54,181 @@ def test_null_item():
     assert(Item("", -1, 0) == items[0])
 
 
-def test_random_named_item_with_quality_gt_0():
-    items = [Item("Foo", 0, 10)]
-
-    gilded_rose = GildedRose(items)
-    gilded_rose.update_quality()
-
-    assert(Item("Foo", -1, 8) == items[0])
-
-
 def test_item_repr():
     item = Item("bar", 1, 2)
 
     assert("bar, 1, 2" == str(item))
 
 
-def test_aged_brie_named_item_with_quality_eq_50():
-    items = [Item("Aged Brie", 0, 50)]
+def test_sulfuras_named_item_dont_change():
+    random.seed(1)
+    sell_in = random.randint(-100, 100)
+    quality = random.randint(-100, 100)
+    items = [Item("Sulfuras, Hand of Ragnaros", sell_in, quality)]
 
     gilded_rose = GildedRose(items)
     gilded_rose.update_quality()
 
-    assert(Item("Aged Brie", -1, 50) == items[0])
+    assert(Item("Sulfuras, Hand of Ragnaros", sell_in, quality) == items[0])
 
 
-def test_aged_brie_named_item_with_quality_gt_50():
-    items = [Item("Aged Brie", 0, 51)]
-
-    gilded_rose = GildedRose(items)
-    gilded_rose.update_quality()
-
-    assert(Item("Aged Brie", -1, 51) == items[0])
-
-
-def test_aged_brie_named_item_with_quality_lt_50():
-    items = [Item("Aged Brie", 0, 45)]
+def test_any_name_item_decrements_sell_in():
+    random.seed(2)
+    quality = random.randint(-100, 100)
+    sell_in = random.randint(-100, 100)
+    items = [
+        Item("Aged Brie", sell_in, quality),
+        Item("Backstage passes to a TAFKAL80ETC concert", sell_in, quality),
+        Item("Foo", sell_in, quality),
+    ]
 
     gilded_rose = GildedRose(items)
     gilded_rose.update_quality()
 
-    assert(Item("Aged Brie", -1, 47) == items[0])
+    assert(items[0].sell_in == sell_in - 1)
+    assert(items[1].sell_in == sell_in - 1)
+    assert(items[2].sell_in == sell_in - 1)
 
 
-def test_backstage_named_item_with_quality_lt_50():
-    items = [Item("Backstage passes to a TAFKAL80ETC concert", 5, 47)]
-
-    gilded_rose = GildedRose(items)
-    gilded_rose.update_quality()
-
-    assert(Item("Backstage passes to a TAFKAL80ETC concert", 4, 50) == items[0])
-
-
-def test_backstage_named_item_with_sell_in_eq_0():
-    items = [Item("Backstage passes to a TAFKAL80ETC concert", 0, 100)]
+def test_backstage_named_item_with_sell_in_leq_0():
+    random.seed(3)
+    items = [
+        Item("Backstage passes to a TAFKAL80ETC concert",
+             random.randint(-100, 0),
+             random.randint(-100, 100))
+    ]
 
     gilded_rose = GildedRose(items)
     gilded_rose.update_quality()
 
-    assert(Item("Backstage passes to a TAFKAL80ETC concert", -1, 0) == items[0])
+    assert(items[0].quality == 0)
 
 
-def test_sulfuras_named_item():
-    items = [Item("Sulfuras, Hand of Ragnaros", 0, 0)]
-
-    gilded_rose = GildedRose(items)
-    gilded_rose.update_quality()
-
-    assert(Item("Sulfuras, Hand of Ragnaros", 0, 0) == items[0])
-
-
-def test_sulfuras_named_item_with_quality_gt_0():
-    items = [Item("Sulfuras, Hand of Ragnaros", 0, 5)]
+def test_backstage_named_item_with_quality_eq_49():
+    random.seed(4)
+    items = [
+        Item("Backstage passes to a TAFKAL80ETC concert", random.randint(1, 100), 49),
+    ]
 
     gilded_rose = GildedRose(items)
     gilded_rose.update_quality()
 
-    assert(Item("Sulfuras, Hand of Ragnaros", 0, 5) == items[0])
+    assert(items[0].quality == 50)
 
+
+def test_backstage_named_item_with_sell_in_gt_0_and_quality_lt_49():
+    random.seed(4)
+    quality = random.randint(-100, 48)
+    small_sell_in = random.randint(1, 5)
+    avg_sell_in = random.randint(6, 10)
+    big_sell_in = random.randint(11, 100)
+    items = [
+        Item("Backstage passes to a TAFKAL80ETC concert", small_sell_in, quality),
+        Item("Backstage passes to a TAFKAL80ETC concert", avg_sell_in, quality),
+        Item("Backstage passes to a TAFKAL80ETC concert", big_sell_in, quality),
+    ]
+
+    gilded_rose = GildedRose(items)
+    gilded_rose.update_quality()
+
+    assert(items[0].quality == quality + 3)
+    assert(items[1].quality == quality + 2)
+    assert(items[2].quality == quality + 1)
+
+
+def test_backstage_named_item_with_quality_geq_50():
+    random.seed(4)
+    quality = random.randint(50, 100)
+    items = [
+        Item("Backstage passes to a TAFKAL80ETC concert", random.randint(1, 100), quality),
+    ]
+
+    gilded_rose = GildedRose(items)
+    gilded_rose.update_quality()
+
+    assert(items[0].quality == quality)
+
+
+def test_aged_brie_named_item_with_quality_eq_49():
+    random.seed(5)
+    items = [
+        Item("Aged Brie", random.randint(-100, 100), 49),
+    ]
+
+    gilded_rose = GildedRose(items)
+    gilded_rose.update_quality()
+
+    assert(items[0].quality == 50)
+
+
+def test_aged_brie_named_item_with_quality_lt_49():
+    random.seed(5)
+    quality = random.randint(-100, 48)
+    items = [
+        Item("Aged Brie", random.randint(-100, 0), quality),
+        Item("Aged Brie", random.randint(1, 100), quality)
+    ]
+
+    gilded_rose = GildedRose(items)
+    gilded_rose.update_quality()
+
+    assert(items[0].quality == quality + 2)
+    assert(items[1].quality == quality + 1)
+
+
+def test_aged_brie_named_item_with_quality_geq_50():
+    random.seed(5)
+    quality = random.randint(50, 100)
+    items = [
+        Item("Aged Brie", random.randint(-100, 100), quality),
+    ]
+
+    gilded_rose = GildedRose(items)
+    gilded_rose.update_quality()
+
+    assert(items[0].quality == quality)
+
+
+def test_other_named_item_with_quality_eq_1():
+    random.seed(6)
+    items = [
+        Item("Foo", random.randint(-100, 0), 1),
+        Item("Bar", random.randint(1, 100), 1)
+    ]
+
+    gilded_rose = GildedRose(items)
+    gilded_rose.update_quality()
+
+    assert(items[0].quality == 0)
+    assert(items[1].quality == 0)
+
+
+def test_other_named_item_with_quality_gt_1():
+    random.seed(6)
+    quality = random.randint(2, 100)
+    items = [
+        Item("Foo", random.randint(-100, 0), quality),
+        Item("Bar", random.randint(1, 100), quality)
+    ]
+
+    gilded_rose = GildedRose(items)
+    gilded_rose.update_quality()
+
+    assert(items[0].quality == quality - 2)
+    assert(items[1].quality == quality - 1)
+
+
+def test_other_named_item_with_quality_leq_0():
+    random.seed(7)
+    quality = random.randint(-100, 0)
+    sell_in = random.randint(-100, 100)
+    items = [
+        Item("Foo", sell_in, quality),
+        Item("Bar", sell_in, quality)
+    ]
+
+    gilded_rose = GildedRose(items)
+    gilded_rose.update_quality()
+
+    assert(items[0].quality == quality)
+    assert(items[1].quality == quality)
