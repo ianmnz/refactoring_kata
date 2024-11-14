a7eb33cf87012a241c82c836fc6e88baa28fb040
ianmnz <ianmemar@gmail.com>
2024-11-14 15:28:53 +0100

    Refactored update_item_quality

--- gilded_rose/refactored.py
+++ gilded_rose/refactored.py
@@ -14,35 +14,45 @@ class Item:
 
 class GildedRose(object):
 
-    def __init__(self, items: List[Item]):
+    def __init__(self, items: List[Item]) -> None:
         self.items = items
 
-    def update_quality(self):
+    def update_quality(self) -> None:
         for item in self.items:
-            if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
-                if item.quality > 0:
-                    if item.name != "Sulfuras, Hand of Ragnaros":
-                        item.quality = item.quality - 1
-            else:
-                if item.quality < 50:
-                    item.quality = item.quality + 1
-                    if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                        if item.sell_in < 11:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-                        if item.sell_in < 6:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-            if item.name != "Sulfuras, Hand of Ragnaros":
-                item.sell_in = item.sell_in - 1
+            self.update_item_quality(item)
+
+    def update_item_quality(self, item: Item) -> None:
+        if item.name == "Sulfuras, Hand of Ragnaros":
+            return
+
+        item.sell_in -= 1
+
+        if item.name == "Aged Brie":
+            if item.quality < 50:
+                item.quality += 1
+
+                if item.sell_in < 0:
+                    item.quality = min(50, item.quality + 1)
+
+        elif item.name == "Backstage passes to a TAFKAL80ETC concert":
             if item.sell_in < 0:
-                if item.name != "Aged Brie":
-                    if item.name != "Backstage passes to a TAFKAL80ETC concert":
-                        if item.quality > 0:
-                            if item.name != "Sulfuras, Hand of Ragnaros":
-                                item.quality = item.quality - 1
-                    else:
-                        item.quality = item.quality - item.quality
-                else:
-                    if item.quality < 50:
-                        item.quality = item.quality + 1
+                item.quality = 0
+
+            elif item.quality < 50:
+                item.quality += 1
+
+                d_quality = 0
+                if item.sell_in < 5:
+                    d_quality = 2
+
+                elif item.sell_in < 10:
+                    d_quality = 1
+
+                item.quality = min(50, item.quality + d_quality)
+
+        else:   # Any other name
+            if item.quality > 0:
+                item.quality -= 1
+
+                if item.sell_in < 0:
+                    item.quality = max(0, item.quality - 1)
